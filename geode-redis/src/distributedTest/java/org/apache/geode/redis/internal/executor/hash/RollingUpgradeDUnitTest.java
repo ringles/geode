8475d1801c36cfe2a66f174050e5d833084c2a97
Ray Ingles <ringles@vmware.com>
2021-03-23 13:59:50 -0400

    WIP index out of bounds

--- geode-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/hash/RollingUpgradeDUnitTest.java
+++ geode-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/hash/RollingUpgradeDUnitTest.java
@@ -19,7 +19,6 @@ package org.apache.geode.redis.internal.executor.hash;
 import static org.apache.geode.distributed.ConfigurationProperties.MAX_WAIT_TIME_RECONNECT;
 import static org.apache.geode.test.dunit.IgnoredException.addIgnoredException;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -69,7 +68,7 @@ public class RollingUpgradeDUnitTest {
   private static MemberVM server3;
   private static MemberVM server4;
 
-  private static int[] redisPorts;
+  private static String[] redisPortStrings = new String[4];
 
   private StatefulRedisConnection<String, String>[] connections = new StatefulRedisConnection[4];
   private RedisClient[] redisClients = new RedisClient[4];
@@ -79,7 +78,10 @@ public class RollingUpgradeDUnitTest {
 
   @BeforeClass
   public static void classSetup() throws Exception {
-    redisPorts = AvailablePortHelper.getRandomAvailableTCPPorts(4);
+    int[] redisPorts = AvailablePortHelper.getRandomAvailableTCPPorts(4);
+    for (int i = 0; i < 4; i++) {
+      redisPortStrings[i] = "" + redisPorts[i];
+    }
     locatorProperties = new Properties();
     locatorProperties.setProperty(MAX_WAIT_TIME_RECONNECT, "15000");
 
@@ -93,21 +95,17 @@ public class RollingUpgradeDUnitTest {
   @Before
   public void before() {
     addIgnoredException(FunctionException.class);
-    String redisPort1 = "" + redisPorts[0];
-    String redisPort2 = "" + redisPorts[1];
-    String redisPort3 = "" + redisPorts[2];
-    String redisPort4 = "" + redisPorts[3];
     // For now only tell the client about redisPort1.
     // That server is never restarted so clients should
     // never fail due to the server they are connected to failing.
     DUnitSocketAddressResolver dnsResolver1 =
-        new DUnitSocketAddressResolver(new String[]{redisPort1});
+        new DUnitSocketAddressResolver(new String[]{redisPortStrings[0]});
     DUnitSocketAddressResolver dnsResolver2 =
-        new DUnitSocketAddressResolver(new String[]{redisPort2});
+        new DUnitSocketAddressResolver(new String[]{redisPortStrings[1]});
     DUnitSocketAddressResolver dnsResolver3 =
-        new DUnitSocketAddressResolver(new String[]{redisPort3});
+        new DUnitSocketAddressResolver(new String[]{redisPortStrings[2]});
     DUnitSocketAddressResolver dnsResolver4 =
-        new DUnitSocketAddressResolver(new String[]{redisPort4});
+        new DUnitSocketAddressResolver(new String[]{redisPortStrings[3]});
 
     ClientResources resources1 = ClientResources.builder()
         .socketAddressResolver(dnsResolver1)
@@ -144,7 +142,7 @@ public class RollingUpgradeDUnitTest {
 
   @After
   public void after() {
-    for (int i = 0; i < 4; i++) {
+    for (int i = 1; i < 4; i+=2) {
       connections[i].close();
       redisClients[i].shutdown();
     }
@@ -171,10 +169,10 @@ public class RollingUpgradeDUnitTest {
   }
 
   private void modifyDataWhileCrashingVMs(DataType dataType) throws Exception {
-    AtomicBoolean running1 = new AtomicBoolean(false);
-    AtomicBoolean running2 = new AtomicBoolean(false);
-    AtomicBoolean running3 = new AtomicBoolean(false);
-    AtomicBoolean running4 = new AtomicBoolean(false);
+    AtomicBoolean running1 = new AtomicBoolean(true);
+    AtomicBoolean running2 = new AtomicBoolean(true);
+    AtomicBoolean running3 = new AtomicBoolean(true);
+    AtomicBoolean running4 = new AtomicBoolean(true);
 
     Runnable task1 = null;
     Runnable task2 = null;
@@ -183,69 +181,68 @@ public class RollingUpgradeDUnitTest {
 
     switch (dataType) {
       case HSET:
-        task1 = () -> hsetPerformAndVerify(0, 20000, running1);
-        task2 = () -> hsetPerformAndVerify(1, 20000, running2);
-        task3 = () -> hsetPerformAndVerify(3, 20000, running3);
-        task4 = () -> hsetPerformAndVerify(4, 1000, running4);
+        task1 = () -> hsetPerformAndVerify(0, running1);
+        task2 = () -> hsetPerformAndVerify(1, running2);
+        task3 = () -> hsetPerformAndVerify(3, running3);
+        task4 = () -> hsetPerformAndVerify(4, running4);
         break;
       case SADD:
-        task1 = () -> saddPerformAndVerify(0, 20000, running1);
-        task2 = () -> saddPerformAndVerify(1, 20000, running2);
-        task3 = () -> saddPerformAndVerify(3, 20000, running3);
-        task4 = () -> saddPerformAndVerify(4, 1000, running4);
+        task1 = () -> saddPerformAndVerify(0, running1);
+        task2 = () -> saddPerformAndVerify(1, running2);
+        task3 = () -> saddPerformAndVerify(3, running3);
+        task4 = () -> saddPerformAndVerify(4, running4);
         break;
       case SET:
-        task1 = () -> setPerformAndVerify(0, 20000, running1);
-        task2 = () -> setPerformAndVerify(1, 20000, running2);
-        task3 = () -> setPerformAndVerify(3, 20000, running3);
-        task4 = () -> setPerformAndVerify(4, 1000, running4);
+        task1 = () -> setPerformAndVerify(0, running1);
+        task2 = () -> setPerformAndVerify(1, running2);
+        task3 = () -> setPerformAndVerify(3, running3);
+        task4 = () -> setPerformAndVerify(4, running4);
         break;
     }
 
-    Future<Void> future1 = executor.runAsync(task1);
-    Future<Void> future2 = executor.runAsync(task2);
-    Future<Void> future3 = executor.runAsync(task3);
-    Future<Void> future4 = executor.runAsync(task4);
-
-    server1.stop();
-    server1 = cluster.startRedisVM(1, redisPorts[0], locator.getPort());
-    running1.set(true);
-    rebalanceAllRegions(server1);
+//    server1.stop();
+//    server1 = cluster.startRedisVM(1, redisPortStrings[0], locator.getPort());
+//    rebalanceAllRegions(server1);
+//    Thread.sleep(2000);
+//    Future<Void> future1 = executor.runAsync(task1);
 
     server2.stop();
-    server2 = cluster.startRedisVM(2, redisPorts[1], locator.getPort());
-    running2.set(true);
+    server2 = cluster.startRedisVM(2, redisPortStrings[1], locator.getPort());
     rebalanceAllRegions(server2);
+    Thread.sleep(2000);
+    Future<Void> future2 = executor.runAsync(task2);
 
-    server3.stop();
-    server3 = cluster.startRedisVM(3, redisPorts[2], locator.getPort());
-    running3.set(true);
-    rebalanceAllRegions(server3);
+//    server3.stop();
+//    server3 = cluster.startRedisVM(3, redisPortStrings[2], locator.getPort());
+//    rebalanceAllRegions(server3);
+//    Thread.sleep(2000);
+//    Future<Void> future3 = executor.runAsync(task3);
 
     server4.stop();
-    server4 = cluster.startRedisVM(4, redisPorts[3], locator.getPort());
-    running4.set(true);
+    server4 = cluster.startRedisVM(4, redisPortStrings[3], locator.getPort());
     rebalanceAllRegions(server4);
+    Thread.sleep(2000);
+    Future<Void> future4 = executor.runAsync(task4);
 
     running1.set(false);
     running2.set(false);
     running3.set(false);
     running4.set(false);
 
-    future1.get();
+//    future1.get();
     future2.get();
-    future3.get();
+//    future3.get();
     future4.get();
   }
 
-  private void hsetPerformAndVerify(int index, int minimumIterations, AtomicBoolean isRunning) {
+  private void hsetPerformAndVerify(int index, AtomicBoolean isRunning) {
     String key = "hset-key-" + index;
     int iterationCount = 0;
 
     connections[index] = redisClients[index].connect();
     RedisCommands<String, String> commands = connections[index].sync();
 
-    while (iterationCount < minimumIterations || isRunning.get()) {
+    while (isRunning.get()) {
       String fieldName = "field-" + iterationCount;
       try {
         commands.hset(key, fieldName, "value-" + iterationCount);
@@ -269,14 +266,14 @@ public class RollingUpgradeDUnitTest {
     logger.info("--->>> HSET test ran {} iterations", iterationCount);
   }
 
-  private void saddPerformAndVerify(int index, int minimumIterations, AtomicBoolean isRunning) {
+  private void saddPerformAndVerify(int index, AtomicBoolean isRunning) {
     String key = "sadd-key-" + index;
     int iterationCount = 0;
 
     connections[index] = redisClients[index].connect();
     RedisCommands<String, String> commands = connections[index].sync();
 
-    while (iterationCount < minimumIterations || isRunning.get()) {
+    while (isRunning.get()) {
       String member = "member-" + index + "-" + iterationCount;
       try {
         commands.sadd(key, member);
@@ -303,13 +300,13 @@ public class RollingUpgradeDUnitTest {
     logger.info("--->>> SADD test ran {} iterations, retrying {} times", iterationCount);
   }
 
-  private void setPerformAndVerify(int index, int minimumIterations, AtomicBoolean isRunning) {
+  private void setPerformAndVerify(int index, AtomicBoolean isRunning) {
     int iterationCount = 0;
 
     connections[index] = redisClients[index].connect();
     RedisCommands<String, String> commands = connections[index].sync();
 
-    while (iterationCount < minimumIterations || isRunning.get()) {
+    while (isRunning.get()) {
       String key = "set-key-" + index + "-" + iterationCount;
       try {
         commands.set(key, key);
